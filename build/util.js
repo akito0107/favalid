c88a7757dfe07205d8cfba238e6307a08bc31544
Akito Ito <akito0107@gmail.com>
2018-03-01 01:51:47 +0900

    fix: remove build

--- build/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _ = require("lodash");
-exports.strlen = (str) => [...str].length;
-exports.isBlank = s => {
-    return (_.isEmpty(s) && !_.isNumber(s)) || _.isNaN(s);
-};
-//# sourceMappingURL=util.js.map
