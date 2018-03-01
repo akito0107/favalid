0000738c3f65c38c00538f914e13eab24445bf33
Akito Ito <akito0107@gmail.com>
2018-03-02 01:57:13 +0900

    refactor: strings test

--- src/validators/__tests__/strings.test.ts
+++ src/validators/__tests__/strings.test.ts
@@ -1,78 +1,67 @@
 import * as assert from "power-assert";
 import { maxLength, minLength, regexp } from "../strings";
 
+function helper(tester, expect) {
+  assert.deepStrictEqual(tester(), expect);
+}
+
 describe("strings", () => {
   test("minLength", () => {
-    const messager = () => "";
-    const result = minLength("aaa", 2, messager)();
-    assert.deepStrictEqual(result, {
+    helper(minLength("aaa", 2, () => ""), {
       error: false,
       message: ""
     });
   });
   test("minLength:fail", () => {
-    const messager = () => "error";
-    const result = minLength("aaa", 5, messager)();
-    assert.deepStrictEqual(result, {
+    helper(minLength("aaa", 5, () => "error"), {
       error: true,
       message: "error"
     });
   });
-
   test("maxLength", () => {
-    const messager = () => "";
-    const result = maxLength("aaabb", 10, messager)();
-    assert.deepStrictEqual(result, {
+    helper(maxLength("aaabb", 10, () => ""), {
       error: false,
       message: ""
     });
   });
   test("maxLength:fail", () => {
-    const messager = () => "error";
-    const result = maxLength("aaa", 2, messager)();
-    assert.deepStrictEqual(result, {
+    helper(maxLength("aaa", 2, () => "error"), {
       error: true,
       message: "error"
     });
   });
-
   test("regexp", () => {
-    const messger = () => "";
-    const result = regexp("abcdef", /abcdef/, messger, {})();
-    assert.deepStrictEqual(result, {
+    helper(regexp("abcdef", /abcdef/, () => "", {}), {
       error: false,
       message: ""
     });
   });
-
   test("regexp:fail", () => {
-    const messger = () => "error";
-    const result = regexp("asdef", /abcdef/, messger, {})();
-    assert.deepStrictEqual(result, {
+    helper(regexp("asdef", /abcdef/, () => "error", {}), {
       error: true,
       message: "error"
     });
   });
-
   test("regexp:exclude", () => {
-    const messager = () => "";
-    const result = regexp("全角カタカナのみ", /[ｦ-ﾟ]/, messager, {
-      exclude: true
-    })();
-    assert.deepStrictEqual(result, {
-      error: false,
-      message: ""
-    });
+    helper(
+      regexp("全角カタカナのみ", /[ｦ-ﾟ]/, () => "", {
+        exclude: true
+      }),
+      {
+        error: false,
+        message: ""
+      }
+    );
   });
-
   test("regexp:exclude:fail", () => {
-    const messager = () => "error";
-    const result = regexp("半角ｶﾀｶﾅまざってる", /[ｦ-ﾟ]/, messager, {
-      exclude: true
-    })();
-    assert.deepStrictEqual(result, {
-      error: true,
-      message: "error"
-    });
+    helper(
+      regexp("半角ｶﾀｶﾅまざってる", /[ｦ-ﾟ]/, () => "error", {
+        exclude: true
+      }),
+      {
+        error: true,
+        message: "error"
+      }
+    );
   });
 });
