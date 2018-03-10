122d6f3cca5174e80cd70b7d2cbaa7b72c4a8a27
Akito Ito <akito0107@gmail.com>
2018-03-10 12:00:02 +0900

    feat: add validator funcs to numbers

--- src/validators/__tests__/numbers.test.ts
+++ src/validators/__tests__/numbers.test.ts
@@ -1,38 +1,136 @@
-import * as assert from "power-assert";
-import { max, min } from "../numbers";
+import {
+  integer,
+  lessThan,
+  max,
+  min,
+  moreThan,
+  negative,
+  positive
+} from "../numbers";
+import { helper } from "./helper";
 
 describe("number validator", () => {
   test("min", () => {
-    const messager = () => "error";
-    const result = min(2, messager)(1);
-    assert.deepStrictEqual(result, {
+    helper(min(2, () => "error"), 1, {
       error: false,
       message: ""
     });
   });
 
+  test("min:boundary", () => {
+    helper(min(2, () => "error"), 2, {
+      error: true,
+      message: "error"
+    });
+  });
+
   test("min:fail", () => {
-    const messager = () => "error";
-    const result = min(1, messager)(2);
-    assert.deepStrictEqual(result, {
+    helper(min(1, () => "error"), 2, {
       error: true,
       message: "error"
     });
   });
 
   test("max", () => {
-    const messager = () => "error";
-    const result = max(1, messager)(2);
-    assert.deepStrictEqual(result, {
+    helper(max(1, () => "error"), 2, {
       error: false,
       message: ""
     });
   });
 
+  test("max:boundary", () => {
+    helper(max(2, () => "error"), 2, {
+      error: true,
+      message: "error"
+    });
+  });
+
   test("max:fail", () => {
-    const messager = () => "error";
-    const result = max(3, messager)(2);
-    assert.deepStrictEqual(result, {
+    helper(max(3, () => "error"), 2, {
+      error: true,
+      message: "error"
+    });
+  });
+
+  test("lessThan", () => {
+    helper(lessThan(3, () => "error"), 2, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("lessThan:boundary", () => {
+    helper(lessThan(3, () => "error"), 3, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("lessThan:fail", () => {
+    helper(lessThan(3, () => "error"), 4, {
+      error: true,
+      message: "error"
+    });
+  });
+
+  test("moreThan", () => {
+    helper(moreThan(3, () => "error"), 4, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("moreThan:boundary", () => {
+    helper(moreThan(3, () => "error"), 3, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("moreThan:fail", () => {
+    helper(moreThan(3, () => "error"), 2, {
+      error: true,
+      message: "error"
+    });
+  });
+
+  test("integer", () => {
+    helper(integer(() => "error"), 2, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("integer:fail", () => {
+    helper(integer(() => "error"), 2.1, {
+      error: true,
+      message: "error"
+    });
+  });
+
+  test("positive", () => {
+    helper(positive(() => "error"), 1, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("positive:fail", () => {
+    helper(positive(() => "error"), -1, {
+      error: true,
+      message: "error"
+    });
+  });
+
+  test("negative", () => {
+    helper(negative(() => "error"), -1, {
+      error: false,
+      message: ""
+    });
+  });
+
+  test("negative:fail", () => {
+    helper(negative(() => "error"), 1, {
       error: true,
       message: "error"
     });
--- src/validators/numbers.ts
+++ src/validators/numbers.ts
@@ -5,7 +5,7 @@ export const min: (limit: number, messager: Messager) => Validator = (
   messager: Messager
 ) => {
   return tester((v: number) => {
-    return v <= limit;
+    return v < limit;
   }, messager);
 };
 
@@ -15,5 +15,41 @@ export const max: (limit: number, messager: Messager) => Validator = (
 ) => {
   return tester((v: number) => {
     return v > limit;
+  }, messager);
+};
+
+export const lessThan: (max: number, messager: Messager) => Validator = (
+  limit,
+  messager
+) => {
+  return tester((v: number) => {
+    return v <= limit;
+  }, messager);
+};
+
+export const moreThan: (min: number, messager: Messager) => Validator = (
+  limit,
+  messager
+) => {
+  return tester((v: number) => {
+    return v >= limit;
+  }, messager);
+};
+
+export const integer: (messager: Messager) => Validator = messager => {
+  return tester((v: number) => {
+    return Number.isInteger(v);
+  }, messager);
+};
+
+export const positive: (messager: Messager) => Validator = messager => {
+  return tester((v: number) => {
+    return v > 0;
+  }, messager);
+};
+
+export const negative: (messager: Messager) => Validator = messager => {
+  return tester((v: number) => {
+    return v < 0;
   }, messager);
 };
