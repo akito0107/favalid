c98cd90c53f292e1a9af728d548b5f395f96f519
MaxMEllon <maxmellon1994@gmail.com>
2018-12-06 17:06:48 +0900

    refactor: drop ramda
    test: Add tests of utils/isBlank
    
    chore: fix comments

--- src/validators/required.ts
+++ src/validators/required.ts
@@ -1,11 +1,10 @@
-import { trim } from "ramda";
 import { Messager, tester, Validator } from "../core";
 import { isBlank, isString } from "../util";
 
 export default (messager: Messager): Validator => {
   return tester(v => {
     if (isString(v)) {
-      v = trim(v);
+      v = v.trim();
     }
     return !isBlank(v);
   }, messager);
