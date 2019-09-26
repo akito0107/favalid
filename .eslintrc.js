6dbdfd7f4bb4e57493b3092daeb90186337e8cc4
Akito Ito <akito0107@gmail.com>
2019-09-26 18:58:08 +0900

    feat: mv to typescript-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./esm/tsconfig.json"
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", {
+      "vars": "all",
+      "args": "after-used",
+      "ignoreRestSiblings": false
+    }],
+    "@typescript-eslint/adjacent-overload-signatures": "error"
+  }
+};
